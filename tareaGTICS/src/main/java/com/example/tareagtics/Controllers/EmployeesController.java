0fbe9c55583152fe31dd4e93cacd6e23a27b115e
dodo240703 <a20202396@pucp.edu.pe>
2024-09-07 04:48:06 -0500

    a

--- tareaGTICS/src/main/java/com/example/tareagtics/Controllers/EmployeesController.java
+++ tareaGTICS/src/main/java/com/example/tareagtics/Controllers/EmployeesController.java
@@ -5,9 +5,12 @@ import com.example.tareagtics.Models.Repositories.EmployeeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/employee")
@@ -26,4 +29,26 @@ public class EmployeesController {
         model.addAttribute("listaEmployees", listaEmployees);
         return "listaEmpleados";
     }
+
+    @GetMapping("/editar")
+    public String editarEmployees(Model model,
+                                  @RequestParam("idEmployee") int id) {
+        Optional<Employee> optionalEmployee = employeesRepository.findById(id);
+
+        if(optionalEmployee.isPresent()) {
+            Employee employee = optionalEmployee.get();
+            model.addAttribute("employee", employee);
+            return "infoEmpleado";
+        }else{
+            return "redirect:/employee/listar";
+        }
+    }
+
+    @PostMapping("/guardar")
+    public String guardar (Employee employee) {
+        employeesRepository.save(employee);
+        return "redirect:/employee/listar";
+    }
+
+
 }
---
+++ tareaGTICS/src/main/java/com/example/tareagtics/Models/Entities/Job.java
@@ -0,0 +1,4 @@
+package com.example.tareagtics.Models.Entities;
+
+public class Job {
+}
